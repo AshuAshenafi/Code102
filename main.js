85610a846c8cd47f175bf37f3506ce17ddaa1124
AshuAshenafi <ashuashenafi@gmail.com>
2019-02-16 05:00:29 +0300

    added

---
+++ main.js
@@ -0,0 +1,15 @@
+/*let name = "Ashenafi";
+let age = 60;
+
+if(age > 21){
+    alert("Hey " + name + ", You are an Adult!");
+}
+else
+alert(name + "! You need to grow.");
+*/
+let speedLimit = 15;
+let currentSpeed = 45;
+
+if(currentSpeed > speedLimit)
+alert("Take a Picture!")
+else alert("Keep it up!")
